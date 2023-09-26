36fb67ac1a32cd6caed4137b1ad555e1bd1b58e0
aditya-xq <adityavivek1998@gmail.com>
2023-09-27 03:40:19 +0530

    batch processing

--- src/convert.py
+++ src/convert.py
@@ -1,6 +1,7 @@
 import logging
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
+from typing import List
 from tqdm import tqdm
 from time import time
 from pathlib import Path
@@ -20,21 +21,25 @@ from utils import (
 # Create a logger for this module
 logger = logging.getLogger(__name__)
 
-def worker(file_path: Path, source_folder: Path, output_folder: Path, quality: int, format: Enum):
+def worker(batch_of_files: List[Path], source_folder: Path, output_folder: Path, quality: int, format: Enum):
     """
-    Process a single file and log any errors.
-    :param file_path: Path of the file to be processed.
+    Process a batch of files and log any errors.
+    :param batch_of_files: List of Paths of the files to be processed.
     :param source_folder: Source folder path.
     :param output_folder: Output folder path.
     :param quality: Quality for the conversion.
     :param format: Format to convert the image to.
     """
-    try:
-        return process_file(file_path, source_folder, output_folder, quality, format.value)
-    except OperationCancelledError as e:
-        logger.error(f"Operation cancelled while processing {file_path}: {e}")
-    except Exception as e:
-        logger.error(f"Error processing {file_path}: {e}")
+    batch_stats = {'total_original_size': 0, 'total_converted_size': 0, 'conversion_count': 0}
+    for file_path in batch_of_files:
+        try:
+            original_size, converted_size, files_converted = process_file(file_path, source_folder, output_folder, quality, format.value)
+            batch_stats = update_conversion_stats(batch_stats, original_size, converted_size, files_converted)
+        except OperationCancelledError as e:
+            logger.error(f"Operation cancelled while processing {file_path}: {e}")
+        except Exception as e:
+            logger.error(f"Error processing {file_path}: {e}")
+    return batch_stats
 
 def update_conversion_stats(stats, original_size, converted_size, files_converted):
     """
@@ -105,12 +110,15 @@ def convert_images(source_folder_path: Path, quality: int = DEFAULT_QUALITY, thr
 
     start_time = time()
 
+    # Create batches of file paths
+    batch_size = len(file_list) // threads
+    batches_of_files = [file_list[i:i + batch_size] for i in range(0, len(file_list), batch_size)]
+
     with ThreadPoolExecutor(max_workers=threads) as executor:
         worker_partial = partial(worker, source_folder=source_folder_path, output_folder=output_folder, quality=quality,
                                  format=format)
-        for original_size, converted_size, files_converted in tqdm(executor.map(worker_partial, file_list),
-                                                                   total=len(file_list), unit="file"):
-            conversion_stats = update_conversion_stats(conversion_stats, original_size, converted_size, files_converted)
+        for batch_stats in tqdm(executor.map(worker_partial, batches_of_files), total=len(batches_of_files), unit="batch"):
+            conversion_stats = update_conversion_stats(conversion_stats, batch_stats['total_original_size'], batch_stats['total_converted_size'], batch_stats['conversion_count'])
 
     conversion_stats['total_time'] = time() - start_time
     generate_report(conversion_stats)
